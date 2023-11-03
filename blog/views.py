d477dbe3937aceb2e493e76ed118d48359e3e283
MarcioAlex-x <marcioalex586@gmail.com>
2023-11-03 11:11:55 -0300

    Primeiro commit

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Post
+
+def index(request):
+    posts = Post.objects.all()
+    return render(request, 'home.html',{'posts':posts})
+
+def listaPosts(request):
+    lista = Post.objects.all()
+    return render(request, 'listaPosts.html',{'lista':lista})
+
+def detalhePost(request):
+    return render(request, 'detalhePost.html')
